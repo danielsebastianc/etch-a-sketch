fdd06b0f0bd3c904d5793b5d52c267f91bfc804c
DanS <117628985+danielsebastianc@users.noreply.github.com>
2025-02-21 00:17:10 +0700

    add functionality to random color and black button

--- script.js
+++ script.js
@@ -6,19 +6,46 @@ const blackBtn = document.querySelector("#black")
 const width = 800
 const height = 800
 let size = 16
+let currentColorMode = "black"
+
+randomColorBtn.addEventListener("mouseenter", ()=>{
+    randomColorBtn.style.backgroundColor = randomHSLA()
+    randomColorBtn.style.color = "black"
+})
+
+randomColorBtn.addEventListener("mouseleave", ()=>{
+    randomColorBtn.style.backgroundColor = "white"
+})
 
 changeGridBtn.addEventListener("click", ()=>{
-    size = prompt("Change Grid Size(max 100): ")
-    if(size > 100 || Number.isInteger(size) || size <= 0){
+    let newSize = prompt("Change Grid Size(max 100): ")
+    newSize = parseInt(newSize, 10);
+    if(isNaN(newSize) || newSize > 100 || newSize <= 0 || !Number.isInteger(newSize)){
         alert("Invalid Grid Size: Please input a whole number between 1 and 100")
     }else{
+        size = newSize;
         container.removeChild(container.firstChild)
-        createGridBox(size)
+        createGridBox(size, currentColorMode)
     }
 })
 
+blackBtn.addEventListener("click", ()=>{
+    currentColorMode = "black";
+    container.removeChild(container.firstChild)
+    createGridBox(size, currentColorMode)
+})
 
-function createGridBox(size){
+randomColorBtn.addEventListener("click", ()=>{
+    currentColorMode = "random"
+    container.removeChild(container.firstChild)
+    createGridBox(size, currentColorMode)
+})
+
+function randomHSLA(){
+    return `hsla(${~~(360 * Math.random())}, 70%,  72%, 0.8)`
+}
+
+function createGridBox(size, currentColorMode){
     const grid = document.createElement("div")
     grid.setAttribute("class", "grid")
     grid.style.width = `${width}px`;
@@ -32,16 +59,20 @@ function createGridBox(size){
             box.setAttribute("class", "grid-child")
             box.style.width = `${boxWidth}px`;
             box.style.height = `${boxHeight}px`;
-            let opacity = 1
-            box.addEventListener("mouseover", ()=>{
-                opacity -= 0.1
+            let opacity = 0
+            box.addEventListener("mouseover", (e)=>{
+                opacity += 0.1
                 box.style.opacity = opacity
+                if(currentColorMode == "random"){
+                    e.target.style.backgroundColor = randomHSLA()
+                }else{
+                    e.target.style.backgroundColor = "black"
+                }
             })
             grid.appendChild(box)
         }
     }
-
     container.appendChild(grid)
 }
 
-createGridBox(size)
+createGridBox(size, currentColorMode)
